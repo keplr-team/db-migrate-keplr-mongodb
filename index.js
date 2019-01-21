286a0e2eab94e30c81cfef3d4cdfdb010112a0c3
Martin Löper <martin.loeper@nesto-software.de>
2019-01-21 01:20:48 +0100

    cast to string in order to prevent type issues

--- index.js
+++ index.js
@@ -523,7 +523,7 @@ exports.connect = function(config, intern, callback) {
 
   var mongoString;
   
-  if (config.useSrvRecord !== undefined && config.useSrvRecord) {
+  if (config.useSrvRecord !== undefined && String(config.useSrvRecord) == 'true') {
     mongoString = 'mongodb+srv://';
   } else {
     mongoString = 'mongodb://';
@@ -556,7 +556,7 @@ exports.connect = function(config, intern, callback) {
       mongoString += '?' + extraParams.join('&');
   }
 
-  db = config.db || ((config.useSrvRecord !== undefined && config.useSrvRecord) ? new MongoClient(mongoString, { useNewUrlParser: true }) : new MongoClient(new Server(host, port)));
+  db = config.db || ((config.useSrvRecord !== undefined && String(config.useSrvRecord) == 'true') ? new MongoClient(mongoString, { useNewUrlParser: true }) : new MongoClient(new Server(host, port)));
 
 
   callback(null, new MongodbDriver(db, intern, mongoString));
